c52b0f4bec94b44071038483d6dc965a9e13d174
VI\MARTINAZ <Martina.Graeber@quest.com>
2018-01-19 10:33:06 +0100

    Project for Controltests added

---
+++ TestElements/DesignHelper.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace TestElements
+{
+    /// <summary>
+    /// Interaction logic for DesignHelper.xaml
+    /// </summary>
+    public partial class DesignHelper : UserControl
+    {
+        public DesignHelper()
+        {
+            InitializeComponent();
+            CommandBinding cbPaste = new CommandBinding(ApplicationCommands.Paste);
+            cbPaste.Executed += PasteCommandHandler;
+            Box.CommandBindings.Add(cbPaste);
+        }
+
+        private void Shutdown(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        void PasteCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            Box.Selection.Text = Clipboard.GetText();
+            Box.Selection.Select(Box.Selection.End,Box.Selection.End);
+        }
+
+        private void UIElement_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ TestElements/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestElements
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveWindow(object sender, DragDeltaEventArgs e)
+        {
+            Left = Left + e.HorizontalChange;
+            Top = Top + e.VerticalChange;
+        }
+    }
+}
